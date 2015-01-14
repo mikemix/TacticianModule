a187af474c5df75172b164909b1e0143b3309072
Gary Hockin <gary@hock.in>
2015-01-14 18:13:04 +0000

    Fixed travis unit tests

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,7 +1,11 @@
 <?php
-chdir('./../../../');
+chdir('./../../');
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = require('vendor/autoload.php');
 if (!array_key_exists('TacticianModule', $loader->getPrefixes())) {
-    $loader->add('TacticianModule', 'module/TacticianModule/src');
+    $loader->add('TacticianModule', './module/TacticianModule/src');
+}
+
+if(!$loader) {
+    throw new Exception('No Autoloading setup');
 }
