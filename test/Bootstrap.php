692e89060bcb7b7d5769e4e91be7df52f8e8f726
Gary Hockin <gary@hock.in>
2015-01-14 18:59:10 +0000

    Fixed tests AGAIN

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,15 +1,11 @@
 <?php
-if(file_exists('Module.php')) {
-    chdir('./../../');
-}
-
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = require('vendor/autoload.php');
 if(!$loader) {
     throw new Exception('No Autoloading setup');
 }
 if (!array_key_exists('TacticianModule', $loader->getPrefixes())) {
-    $loader->add('TacticianModule', './module/TacticianModule/src');
+    $loader->add('TacticianModule', './src');
 }
 
 
