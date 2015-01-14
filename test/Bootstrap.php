8d112893059e3659abf437fd281ed95bc7cd9ff7
Gary Hockin <gary@hock.in>
2015-01-14 19:17:03 +0000

    Fixed more CS stuff

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = require('vendor/autoload.php');
-if(!$loader) {
+if (!$loader) {
     throw new Exception('No Autoloading setup');
 }
 if (!array_key_exists('TacticianModule', $loader->getPrefixes())) {
     $loader->add('TacticianModule', './src');
 }
-
 
