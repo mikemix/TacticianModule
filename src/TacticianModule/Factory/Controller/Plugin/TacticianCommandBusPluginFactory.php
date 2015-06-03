255a2812358bf68733a77e4ee63d86fe6f09483d
Mike Zukowsky <michal@durooil.com>
2015-06-03 14:41:31 +0200

    Update TacticianCommandBusPluginFactory.php

--- src/TacticianModule/Factory/Controller/Plugin/TacticianCommandBusPluginFactory.php
+++ src/TacticianModule/Factory/Controller/Plugin/TacticianCommandBusPluginFactory.php
@@ -3,22 +3,27 @@ namespace TacticianModule\Factory\Controller\Plugin;
 
 use League\Tactician\CommandBus;
 use TacticianModule\Controller\Plugin\TacticianCommandBusPlugin;
-use Zend\Mvc\Controller\PluginManager;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\AbstractPluginManager;
 
 class TacticianCommandBusPluginFactory implements FactoryInterface
 {
     /**
      * Create service
      *
-     * @param ServiceLocatorInterface|PluginManager $pm
+     * @param ServiceLocatorInterface $pm
      * @return TacticianCommandBusPlugin
      */
     public function createService(ServiceLocatorInterface $pm)
     {
-        /** @var CommandBus $commandBus */
-        $commandBus = $pm->getServiceLocator()->get(CommandBus::class);
+        if ($pm instanceof AbstractPluginManager) {
+            /** @var CommandBus $commandBus */
+            $commandBus = $pm->getServiceLocator()->get(CommandBus::class);
+        } else {
+            /** @var CommandBus $commandBus */
+            $commandBus = $pm->get(CommandBus::class);
+        }
 
         return new TacticianCommandBusPlugin($commandBus);
     }
