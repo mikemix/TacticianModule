1c13630176d506007aef13aa85b422e69d2534a4
mikemix <michal@durooil.com>
2015-07-08 21:52:27 +0200

    PSR-0 to PSR-4

--- src/Module.php
+++ src/Module.php
@@ -5,6 +5,6 @@ class Module
 {
     public function getConfig()
     {
-        return require __DIR__ . '/../../config/module.config.php';
+        return require __DIR__ . '/../config/module.config.php';
     }
 }
